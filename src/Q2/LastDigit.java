c2e1050e18faf12bebd3c8c914d36d7a08668e98
deepika <deepikasenthil60@gmai.com>
2026-06-16 21:39:27 +0530

    Added LastDigit program

--- src/Q2/LastDigit.java
+++ src/Q2/LastDigit.java
@@ -1,4 +1,12 @@
 package Q2;
 
 public class LastDigit {
+     public static void main(String[] args) {
+            int number = 9876;
+
+            int lastDigit = number % 10;
+
+            System.out.println("Last digit = " + lastDigit);
+        }
+
 }
