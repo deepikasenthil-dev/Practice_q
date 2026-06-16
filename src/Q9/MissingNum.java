9b330b995a0d306258b2e69db8fad99bd162cb7d
deepika <deepikasenthil60@gmai.com>
2026-06-16 22:25:21 +0530

    Missing number in an array

---
+++ src/Q9/MissingNum.java
@@ -0,0 +1,24 @@
+package Q9;
+
+public class MissingNum {
+
+        public static void main(String[] args) {
+
+            int[] arr = {1, 2, 3, 5};
+
+            int n = arr.length + 1;
+
+            int total = n * (n + 1) / 2;
+
+            int sum = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                sum = sum + arr[i];
+            }
+
+            int missing = total - sum;
+
+            System.out.println("Missing Number = " + missing);
+        }
+
+}
